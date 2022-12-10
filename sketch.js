2c2d9ae5e340366105c2afb8fb3d75afe87d6c8f
iwashi <minjae.jeon2003@gmail.com>
2022-12-10 22:38:58 +0900

    rect field
    make many rect

--- sketch.js
+++ sketch.js
@@ -1,7 +1,24 @@
+let a;
+let b;
+let scl = 20;
+let w = 400;
+let h = 400;
+
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400, 400, WEBGL);
+  a = w / scl;
+  b = h / scl;  
 }
 
 function draw() {
-  background(220);
+  background(0);
+  
+  for(let x = 0; x < a; x++){
+    for(let y = 0; y < b; y++){
+      rect(x*scl, y*scl, scl, scl);
+    }
+  }
 }
+
+  
