8b5df57fa491ca5400ff5fbf75e5e345673e2133
iwashi <minjae.jeon2003@gmail.com>
2022-12-11 16:34:49 +0900

    add movement
    add movement test

--- sketch.js
+++ sketch.js
@@ -4,20 +4,34 @@ let scl = 20;
 let w = 880;
 let h = 880;
 let land = [];
+let movement = 0;
 
 function setup() {
   createCanvas(800, 800, WEBGL);
   a = w / scl;
-  b = h / scl;  
+  b = h / scl; 
+  
     for(let x = 0; x < a; x++){
       land[x] = [];
       for(let y = 0; y < b; y++){
-        land[x][y] = random(-10, 10);
+        land[x][y] map(noise(xUP, yUP), 0, 1, -10, 10);
       }
     }
 }
 
 function draw() {
+  
+  movement += 0.1;
+  let xUP = 0;
+    for(let x = 0; x < a; x++){
+      let yUP = movement;
+      for(let y = 0; y < b; y++){
+        land[x][y] = map(noise(xUP, yUP), 0, 1, -10, 10);
+        xUP += 0.1;
+      }
+      yUP += 0.1;
+    }  
+  
   frameRate(60);
   background(0);
   translate(-440, 0);
